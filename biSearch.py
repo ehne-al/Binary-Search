60c86913c814aa914b68c6fe4c89d5d7083ba1bd
Helena Rosa <helenarosa2005@gmail.com>
2021-09-16 17:13:31 -0300

    Commit base code

---
+++ biSearch.py
@@ -0,0 +1,11 @@
+def searchBi(arr):
+    low = 0
+    high = len(arr) -1
+    midIndex = (high + low) // 2
+    midNumber = arr[midIndex]
+    #defines essencial parameters
+    print("The middle number of this array is:", midNumber)
+
+numberElements = int(input("Enter number of elements in array : "))
+a = list(map(int,input("\nEnter the numbers : ").strip().split()))[:numberElements]
+searchBi(a)
